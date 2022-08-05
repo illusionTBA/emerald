9a636d117099f0ae8bc2cf4a1a21462d353e7167
illusions <97645434+illusionTBA@users.noreply.github.com>
2022-08-05 16:10:21 -0400

    Add files via upload

---
+++ public/uv/uv.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+self.__uv$config = {
+    prefix: '/~/',
+    bare: "https://incog.dev/bare/",
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
