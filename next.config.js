2c88f289ad776cc98ece6706719708363addeeed
illusionTBA <illusionztba@gmail.com>
2022-10-26 20:25:25 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,13 +11,9 @@ const nextconfig = {
       /* if you have another bare server you would like to use for this
         instead of the built in one just replace the " destination " value (leave the slashes)
       */
-      // {
-      //   source: "/bare/",
-      //   destination: "http://0.0.0.0:7000/",
-      // },
       {
         source: "/bare/",
-        destination: "http://0.0.0.0:3001/",
+        destination: "http://0.0.0.0:7000/",
       },
       {
         source: "/bare/:path*/",
