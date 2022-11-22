7275242f35f519c2dc0470c14fa084ea22acc316
illusionTBA <illusionztba@gmail.com>
2022-11-21 22:12:20 -0500

    Update uv-sw.js

--- public/uv-sw.js
+++ public/uv-sw.js
@@ -1,6 +1,6 @@
-importScripts('/proxies/uv.bundle.js');
-importScripts('/proxies/uv.config.js');
-importScripts('/proxies/uv.sw.js');
+importScripts('/proxies/uv/uv.bundle.js');
+importScripts('/proxies/uv/uv.config.js');
+importScripts('/proxies/uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
