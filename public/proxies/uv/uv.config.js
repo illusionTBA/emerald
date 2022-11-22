90ef85b50da793bd3e3e4966ee14158e3d276fef
illusionTBA <illusionztba@gmail.com>
2022-11-21 21:53:42 -0500

    add new UV version

--- public/proxies/uv/uv.config.js
+++ public/proxies/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/~/uv/',
-    bare: '/bare/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/proxies/uv/uv.handler.js',
-    bundle: '/proxies/uv/uv.bundle.js',
-    config: '/proxies/uv/uv.config.js',
-    sw: '/proxies/uv/uv.sw.js',
+  prefix: '/~/uv/',
+  bare: '/bare/',
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: '/proxies/uv/uv.handler.js',
+  bundle: '/proxies/uv/uv.bundle.js',
+  config: '/proxies/uv/uv.config.js',
+  sw: '/proxies/uv/uv.sw.js',
 };
